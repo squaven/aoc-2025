98daa7e2c814f316e6062a56f0f6e739c9155c4d
bonkeb <brandon@bonke.co>
2025-12-02 09:59:06 -0800

    Initial attempt at puzzle 1 part 2

--- puzzle-1-secret-entrance/src/main.rs
+++ puzzle-1-secret-entrance/src/main.rs
@@ -3,30 +3,44 @@ use std::io::{BufRead, BufReader};
 
 struct Dial {
     position: i32,
+    zero_count: i32
 }
 
 impl Dial {
-    fn new(position: i32) -> Self {
-        Dial { position }
+    fn new() -> Self {
+        Dial {
+            position: 50,
+            zero_count: 0
+        }
     }
 
     fn rotate_right(&mut self, steps: i32) {
-        println!("  DEBUG {0}", self.position);
+        let mut initial_position = self.position.clone();
         self.position = self.position + steps;
-        println!("  DEBUG {0}", self.position);
         while self.position > 99 {
             self.position = self.position - 100;
-            println!("    DEBUG {0}", self.position);
+            if initial_position != 0 && self.position != 0 {
+                self.zero_count = self.zero_count + 1;
+            }
+            initial_position = self.position.clone();
+        }
+        if self.position == 0 {
+            self.zero_count = self.zero_count + 1;
         }
     }
 
     fn rotate_left(&mut self, steps: i32) {
-        println!("  DEBUG {0}", self.position);
+        let mut initial_position = self.position.clone();
         self.position = self.position - steps;
-        println!("  DEBUG {0}", self.position);
         while self.position < 0 {
             self.position = self.position + 100;
-            println!("    DEBUG {0}", self.position);
+            if initial_position != 0 && self.position != 0 {
+                self.zero_count = self.zero_count + 1;
+            }
+            initial_position = self.position.clone();
+        }
+        if self.position == 0 {
+            self.zero_count = self.zero_count + 1
         }
     }
 
@@ -42,30 +56,25 @@ impl Dial {
     fn get_position(&self) -> i32 {
         self.position
     }
+    fn get_count(&self) -> i32 {
+        self.zero_count
+    }
 }
 
 fn main() {
-    let mut dial = Dial::new(50);
+    let mut dial = Dial::new();
 
     let password_result =
         File::open("input.txt")
             .map(|file| BufReader::new(file))
             .map(|reader| {
-                let mut password_counter = 0;
                 for line in reader.lines() {
                     let line = line.unwrap();
                     let initial_dial = dial.get_position();
                     dial.rotate_from_line(&line);
-                    if dial.get_position() == 0 {
-                        password_counter = password_counter + 1;
-                        println!("DEBUG {0} -> {1} -> {2} PASS {3}", initial_dial, line, dial.get_position(), password_counter);
-                    }
-                    else {
-                        println!("DEBUG {0} -> {1} -> {2}", initial_dial, line, dial.get_position());
-                    }
-                    println!("")
+                    println!("DEBUG {0} -> {1} -> {2} PASS {3}", initial_dial, line, dial.get_position(), dial.get_count());
                 }
-                password_counter
+                dial.get_count()
             });
     match password_result {
         Ok(p) => println!("Password {0}" , p.to_string()),
